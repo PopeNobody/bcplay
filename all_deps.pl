5d53aaec00cccdc73dc530bea23d81e866807b44
nobody <github2@invisiblehand.church>
2020-11-03 04:49:44 -0500

    merged.church

--- all_deps.pl
+++ all_deps.pl
@@ -6,10 +6,10 @@ use warnings;
 use Carp qw(cluck);
 use Data::Dumper;
 
+die "usage: $0 [outfile] [depfiles...]" unless @ARGV > 1;
 my $ofn=shift;
-die "usage: $0 [outfile] [depfiles...]" unless @ARGV;
 eval "use autodie qw(:all);";
-open(my $ofh,">$ofn");
+open(STDOUT,">$ofn");
 my %seen;
 while(<ARGV>) {
   s{[\\\s:]}{ }g;
@@ -19,4 +19,4 @@ while(<ARGV>) {
   };
 };
 
-print "$_\n" for sort keys %seen;
+print("$_\n") for sort keys %seen;
