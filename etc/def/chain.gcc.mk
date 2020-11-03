5e894c8a6bd9550fdf98b7ed2fdec4087962e6b1
nobody <github2@invisiblehand.church>
2020-11-03 04:14:18 -0500

    x

---
+++ etc/def/chain.gcc.mk
@@ -0,0 +1,8 @@
+CXX:=g++
+CXXFLAGS = @etc/cxxflags
+CPPFLAGS += -MD -MT $@ @etc/cppflags
+LDFLAGS := 
+LDFLAGS += -L/home/nn/src/bc/lib64
+LDFLAGS += @etc/ld_flags -L$(PWD)/lib
+AR:= ar
+ARFLAGS:= Urv
